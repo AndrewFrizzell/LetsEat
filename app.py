bd270519930ce6f045143fd2431444b3e4a3346a
AndrewFrizzell <andrewfrizzell1991@gmail.com>
2026-04-22 12:16:19 -0500

    add max distance filter

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def home():
     selected_cuisine = request.args.get("cuisine", "")
     max_price = request.args.get("price", "")
     min_rating = request.args.get("rating", "")
+    max_distance = request.args.get("distance", "")
     pick_random = request.args.get("pick")
 
     filtered_restaurants = restaurants
@@ -36,6 +37,14 @@ def home():
             if r["rating"] >= min_rating
         ]
 
+    #max distance filter
+    if max_distance:
+        max_distance = float(max_distance)
+        filtered_restaurants = [
+            r for r in filtered_restaurants
+            if r["distance"] <= max_distance
+        ]
+
     cusisines = sorted(set(r["cuisine"] for r in restaurants))
 
     chosen_restaurant = None
@@ -52,6 +61,7 @@ def home():
          selected_cuisine=selected_cuisine,
          max_price=str(max_price) if max_price else "",
          min_rating=str(min_rating) if min_rating else "",
+         max_distance=str(max_distance) if max_distance else "",
          chosen_restaurant=chosen_restaurant
          )
 
