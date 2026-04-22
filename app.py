753ab98c3ae2aca050a8d07a3551d45fdf2876d7
AndrewFrizzell <andrewfrizzell1991@gmail.com>
2026-04-22 10:00:09 -0500

    Add random restaurant picker feature

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from restaurants import restaurants
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", restaurants=restaurants)
+    selected_cuisine = request.args.get("cuisine", "")
+
+    if selected_cuisine:
+        filtered_restaurants = [
+            restaurant for restaurant in restaurants
+            if restaurant["cuisine"] == selected_cuisine
+        ]
+    else:
+        filtered_restaurants = restaurants
+
+    cusisines = sorted(set(restaurant["cuisine"] for restaurant in restaurants))
+
+    return render_template(
+        "index.html",
+         restaurants=filtered_restaurants,
+         cuisines=cusisines,
+         selected_cuisine=selected_cuisine
+         )
 
 if __name__ == "__main__":
     app.run(debug=True)
